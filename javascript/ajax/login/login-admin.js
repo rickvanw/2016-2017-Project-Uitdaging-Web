ab24e3f9154e7a31637c32393b04b4ae724b8248
maurice_2 <mauricebergboer@hotmail.com>
2017-05-18 10:20:12 +0200

    Ajax calls

--- javascript/ajax/login/login-admin.js
+++ javascript/ajax/login/login-admin.js
@@ -1,3 +1,30 @@
 /**
+ * Ajax call for logging in as an admin
+ *
  * Created by maurice_2 on 17-5-2017.
  */
+$(document).ready(function () {
+
+    $('#login').submit(function () {
+        // Get username and password
+        var username = $('#username').val();
+        var password = $('#password').val();
+
+        $.ajax({
+            type: 'POST',
+            url: "http://localhost:8000/admin/login",
+            dataType: 'json',
+            data: {'userName': username, 'password': password},
+            crossdomain: true,
+            success: function(data){
+                // TODO afhandeling succes
+            },
+            statusCode: {
+                // TODO afhandeling status codes
+            },
+            error: function(err){
+                console.log("Error: " + err);
+            }
+        });
+    });
+});
--- javascript/ajax/register/register-admin.js
+++ javascript/ajax/register/register-admin.js
@@ -1,3 +1,31 @@
 /**
+ * Ajax call for registering as admin
+ *
  * Created by maurice_2 on 17-5-2017.
  */
+$(document).ready(function(){
+
+    $('#register').submit(function () {
+
+        var email = $('#email').val();
+        var first_name = $('#first-name').val();
+        var last_name = $('#last-name').val();
+        var password = $('#password').val();
+
+        $.ajax({
+            type: 'POST',
+            url: "http://localhost:8000/admin/register",
+            dataType: 'json',
+            data: {'email': email, 'first-name': first_name, 'last-name': last_name, 'password': password},
+            success: function () {
+
+            },
+            statusCode: {
+
+            },
+            error: function (err) {
+
+            }
+        });
+    });
+});
