1bf7b1a83a9856575f3c97cae4bfe36fa8ae625c
rickvanw <rickvanweersel@hotmail.com>
2017-05-19 17:16:30 +0200

    Treatment javascript, generating exercise boxes with correct id's

--- javascript/treatment.js
+++ javascript/treatment.js
@@ -1,7 +1,10 @@
 /**
  * Created by rickv on 18-5-2017.
  */
+placeExercises();
+
 $(document).ready(function() {
+
 
     $('.left_arrow').click(function(event){
         alert("previous");
@@ -10,20 +13,19 @@ $(document).ready(function() {
         alert("next");
     });
 
-
     var disabled = 0;
 
-    $('#doneButton').click(function(event){
-        disabled = 1;
+    $('.doneButton').click(function(event){
+        disabled=this.id.substr(this.id.length - 1);
         return false;
     });
 
-    $('#notdoneButton').click(function(event){
+    $('.notdoneButton').click(function(event){
         disabled = 0;
     });
 
-    $('#likeButton').click(function(event){
-        if (disabled==0) {
+    $('.likeButton').click(function(event){
+        if (disabled != this.id.substr(this.id.length - 1)) {
             alert("Geef eerst aan dat je de oefening gedaan hebt");
             return false;
         }else{
@@ -31,9 +33,9 @@ $(document).ready(function() {
         }
     });
 
-    $('#dislikeButton').click(function(event){
-        if (disabled==0) {
-            alert("Geef eerst aan of je de oefening gedaan hebt");
+    $('.dislikeButton').click(function(event){
+        if (disabled != this.id.substr(this.id.length - 1)) {
+            alert("Geef eerst aan dat je de oefening gedaan hebt");
             return false;
         }else{
 
@@ -42,10 +44,43 @@ $(document).ready(function() {
 
 });
 
-function fillNames(names) {
-    document.getElementById('nekbutton').textContent = names[0];
-    document.getElementById('').textContent = names[0];
+function placeExercises() {
 
+        var text = $("#template_exercise").html();
+        for (i = 1; i < 5; i++) {
+
+            //Set all the right unique id's
+            $("#all_exercises_container").append(text);
+            $("#collapse0").attr("id","collapse"+i);
+            $("#link01").attr({href:"#collapse"+i, id:""});
+            $("#link02").attr({href:"#collapse"+i, id:""});
+            $("#doneButton0").attr("id","doneButton"+i);
+            $("#notDoneButton0").attr("id","notDoneButton"+i);
+            $("#likeButton0").attr("id","likeButton"+i);
+            $("#dislikeButton0").attr("id","dislikeButton"+i);
+            $("#exercise0").attr("id","exercise"+i);
+
+            // Fill the exercises
+
+            //Quickview image
+            $('#' + "exercise"+i).find('.quickview_image').attr("src","img/exercise"+i + ".jpg");
+
+            //Expanded image
+            $('#' + "exercise"+i).find('.collapse_image').attr("src","img/exercise"+i + ".jpg");
+
+            // Title
+            $('#' + "exercise"+i).find('.exercise_quickview_title').text("Exercise name"+i);
+
+            //Repeats amount
+            $('#' + "exercise"+i).find('.exercise_quickview_amount_repeats').text(i);
+
+
+            var description = "Planken is niet ingewikkeld. Voor de basisplank ga je eerst op je buik liggen.Plaats je ellebogen onder de schouders en zet je tenen in de vloer. Druk je bovenlichaam omhoog op je onderarmen en til ook je benen van de grond. Vind jehet lastig om in een keer je lijf omhoog te brengen, steun dan als tussenstap op je knieën.";
+
+            //Repeats amount
+            $('#' + "exercise"+i).find('.description_text').text(description+i);
+
+        }
 
 
 }
