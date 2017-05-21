5886c6a27f05347e101e54fb5930ccd48b4caaac
rickvanw <rickvanweersel@hotmail.com>
2017-05-21 23:17:22 +0200

    Treatment user can rate exercise, mark as done, go to different days

--- javascript/treatment.js
+++ javascript/treatment.js
@@ -1,63 +1,116 @@
 /**
  * Created by rickv on 18-5-2017.
  */
+
+//TODO get logged in user auth code
+var jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJ1YmVuYXNzaW5rQGhvdG1haWwuY29tIiwidXNlcl9pZCI6NCwicm9sZV9pZCI6MCwiaWF0IjoxNDk1MzkzNTYwLCJleHAiOjE1MjY5Mjk1NjB9.4UMl25J0i7C4d5METeHxY-4FYrf9ez0B0RkkijuoaCc";
+var done_exercises =[];
+var daysFromCurrentDate = 0;
+getExercises(getCurrentDate());
+
+
 $(document).ready(function() {
-    getExercises();
+    userInteraction();
+    $('.day_text').text(getDateString(getCurrentDate()));
 
 });
 
-$(document).ajaxComplete(function () {
-    var disabled = 0;
+function userInteraction () {
 
-    $('.left_arrow').click(function(event){
-        alert("previous");
+    $('.left_arrow').off("click").on("click", function (e) {
+        e.stopImmediatePropagation();
+        clearExercises();
+        console.log("CLLICk");
+        daysFromCurrentDate--;
+        getExercises(daysFromDate(daysFromCurrentDate));
+        $('.day_text').text(getDateString(daysFromDate(daysFromCurrentDate)));
     });
-    $('.right_arrow').click(function(event){
-        alert("next");
+    $('.right_arrow').off("click").on("click", function (e) {
+        e.stopImmediatePropagation();
+        clearExercises();
+        console.log("CLLICk");
+        daysFromCurrentDate++;
+        getExercises(daysFromDate(daysFromCurrentDate));
+        $('.day_text').text(getDateString(daysFromDate(daysFromCurrentDate)));
     });
 
-    $('.doneButton').click(function(event){
-        //TODO fix buttons
-        alert(this.id.split("doneButton")[1]);
+    $('.doneButton').off("click").on("click", function (e) {
+        e.stopImmediatePropagation();
 
         var buttonExerciseId=this.id.split("doneButton")[1];
+        addToDoneExercises(buttonExerciseId);
+        console.info(done_exercises);
 
-        disabled=buttonExerciseId;
+        doneExercise(1, buttonExerciseId);
+
+        $(this).css('background-color', '#BFE5BF');
+
+        $('#' + "notDoneButton" + buttonExerciseId).css('background-color', 'white');
+
         return false;
     });
 
-    $('.notdoneButton').click(function(event){
-        disabled = 0;
+    $('.notDoneButton').off("click").on("click", function (e) {
+        e.stopImmediatePropagation();
+
+        var buttonExerciseId=this.id.split("notDoneButton")[1];
+        removeFromDoneExercises(buttonExerciseId);
+        console.info(done_exercises);
+
+        doneExercise(-1, buttonExerciseId);
+
+        $(this).css('background-color', '#F99C9C');
+
+        $('#' + "doneButton" + buttonExerciseId).css('background-color', 'white');
+
+        return false;
     });
 
-    $('.likeButton').click(function(event){
-        var buttonExerciseID=this.id.split("likeButton")[1];
+    $('.likeButton').off("click").on("click", function (e) {
+        e.stopImmediatePropagation();
 
-        if (disabled != buttonExerciseID) {
+        var buttonExerciseId=this.id.split("likeButton")[1];
+
+        if (!isInDoneExercises(buttonExerciseId)) {
+            // User should mark exercise as done first
             alert("Geef eerst aan dat je de oefening gedaan hebt");
-            return false;
         }else{
-            rateExercise(1, buttonExerciseID);
+            rateExercise(1, buttonExerciseId);
+            $(this).css('background-color', '#BFE5BF');
+
+            $('#' + "dislikeButton" + buttonExerciseId).css('background-color', 'white');
         }
+        return false;
+
     });
 
-    $('.dislikeButton').click(function(event){
+    $('.dislikeButton').off("click").on("click", function (e) {
+        e.stopImmediatePropagation();
+
         var buttonExerciseId=this.id.split("dislikeButton")[1];
 
-        if (disabled != buttonExerciseId) {
+        if (!isInDoneExercises(buttonExerciseId)) {
+            // User should mark exercise as done first
             alert("Geef eerst aan dat je de oefening gedaan hebt");
-            return false;
         }else{
             rateExercise(-1, buttonExerciseId);
-        }
-    });
-});
+            $(this).css('background-color', '#F99C9C');
 
-function getExercises(){
+            $('#' + "likeButton" + buttonExerciseId).css('background-color', 'white');
+        }
+        return false;
+
+    });
+}
+
+function getExercises(date){
     var request = $.ajax({
         type: 'GET',
+        headers: {
+            'authorization':jwt,
+            'day':date
+        },
         url: "http://localhost:8000" + "/treatment/exercises-day",
-        data: {"checkdate": getCurrentDate()},
         dataType: 'json',
         statusCode: {
             200:function(){
@@ -69,6 +122,9 @@ function getExercises(){
             404: function(error){
                 console.log(404, error)
             }
+        },
+        error: function (err) {
+            alert("Error: " + err);
         }
     });
 
@@ -76,32 +132,6 @@ function getExercises(){
         placeExercises(data)
     });
 
-    request.error(function (jqXHR, textStatus, errorThrown){
-        console.log("ERROR: " + textStatus + " CODE: " + errorThrown);
-    });
-
-    /**
-     * Function for returning the current date.
-     * @returns {Date}
-     */
-    function getCurrentDate(){
-        var currentDate = new Date();
-        var dd = currentDate.getDate();
-        var mm = currentDate.getMonth() + 1; // January is zero, so + 1
-        var yyyy = currentDate.getFullYear();
-
-        // If the days / months are smaller than 10, we want to put a 0 before it. So for example 2017-09-09 instead of 2017-9-9
-        if(dd < 10) {
-            dd = '0' + dd
-        }
-
-        if(mm < 10) {
-            mm = '0' + mm
-        }
-
-        currentDate = yyyy + '/' + mm + '/' + dd;
-        return currentDate;
-    }
 }
 
 function placeExercises(data) {
@@ -109,6 +139,9 @@ function placeExercises(data) {
     var text = $("#template_exercise").html();
 
     data.forEach(function(exercise) {
+
+        console.log("FOREACH");
+
         //Set all the right unique id's
         $("#all_exercises_container").append(text);
         $("#collapse0").attr("id", "collapse" + exercise.exercise_id);
@@ -136,14 +169,38 @@ function placeExercises(data) {
 
         //Repeats amount
         $('#' + "exercise" + exercise.exercise_id).find('.description_text').text(exercise.description);
+
+        //Set previous like/dislike
+        if(exercise.rating_user == 1) {
+            $('#' + "exercise" + exercise.exercise_id).find('.likeButton').css('background-color', '#BFE5BF');
+        }else if(exercise.rating_user == -1){
+            $('#' + "exercise" + exercise.exercise_id).find('.dislikeButton').css('background-color', '#F99C9C');
+        }
+
+        //Set previous done/not done
+        if(exercise.done == 1) {
+            $('#' + "exercise" + exercise.exercise_id).find('.doneButton').css('background-color', '#BFE5BF');
+        }else if(exercise.done == -1){
+            $('#' + "exercise" + exercise.exercise_id).find('.notDoneButton').css('background-color', '#F99C9C');
+        }
+        if (exercise.done == 1) {
+            console.log("ID - " + exercise.exercise_id);
+            addToDoneExercises(exercise.exercise_id)
+        }
+        console.info(done_exercises);
     });
+
+    userInteraction();
 }
 
-function doneExercise() {
+function doneExercise(done, exerciseId) {
     var request = $.ajax({
-        type: 'POST',
-        url: "http://localhost:8000" + "/exercise/rate",
-        data: {"exerciseId": exerciseId, "rating":rating},
+        type: 'PUT',
+        headers: {
+            'authorization':jwt
+        },
+        url: "http://localhost:8000" + "/treatment/exercise-done",
+        data: {"exerciseId": exerciseId, "done":done},
         dataType: 'json',
         statusCode: {
             200:function(){
@@ -154,16 +211,15 @@ function doneExercise() {
             },
             404: function(error){
                 console.log(404, error)
+            },
+            error: function (err) {
+                alert("Error: " + err);
             }
         }
     });
 
     request.done(function (data) {
-        placeExercises(data);
-    });
-
-    request.error(function (jqXHR, textStatus, errorThrown){
-        console.log("ERROR: " + textStatus + " CODE: " + errorThrown);
+        console.log("DONE");
     });
 }
 
@@ -176,6 +232,9 @@ function rateExercise(rating, exerciseId) {
     console.log("rating");
     var request = $.ajax({
         type: 'PUT',
+        headers: {
+            'authorization':jwt
+        },
         url: "http://localhost:8000" + "/exercise/rate",
         data: {"exerciseId": exerciseId, "rating":rating},
         dataType: 'json',
@@ -189,15 +248,105 @@ function rateExercise(rating, exerciseId) {
             404: function(error){
                 console.log(404, error)
             }
-        }
-        ,
+        },
         error: function (err) {
             alert("Error: " + err);
         }
     });
 
     request.done(function (data) {
-        console.log("DONE")
+        console.log("DONE");
     });
 
+}
+
+/**
+ * Function for returning the current date.
+ * @returns {Date}
+ */
+function getCurrentDate(){
+    var currentDate = new Date();
+    var dd = currentDate.getDate();
+    var mm = currentDate.getMonth() + 1; // January is zero, so + 1
+    var yyyy = currentDate.getFullYear();
+
+    // If the days / months are smaller than 10, we want to put a 0 before it. So for example 2017-09-09 instead of 2017-9-9
+    if(dd < 10) {
+        dd = '0' + dd
+    }
+
+    if(mm < 10) {
+        mm = '0' + mm
+    }
+
+    currentDate = yyyy + '-' + mm + '-' + dd;
+    return currentDate;
+}
+
+function daysFromDate(daysBack) {
+    var today = new Date();
+    var yesterday = new Date(today);
+    yesterday.setDate(today.getDate() + daysBack);
+
+    var dd = yesterday.getDate();
+    var mm = yesterday.getMonth() + 1; // January is zero, so + 1
+    var yyyy = yesterday.getFullYear();
+
+    // If the days / months are smaller than 10, we want to put a 0 before it. So for example 2017-09-09 instead of 2017-9-9
+    if(dd < 10) {
+        dd = '0' + dd
+    }
+
+    if(mm < 10) {
+        mm = '0' + mm
+    }
+
+    return yyyy + '-' + mm + '-' + dd;
+}
+
+function getDateString(date){
+
+    var weekday = ["Zondag","Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag"];
+
+    date = new Date(date);
+
+    var dd = date.getDate();
+    var mm = date.getMonth() + 1; // January is zero, so + 1
+    var yyyy = date.getFullYear();
+
+    // If the days / months are smaller than 10, we want to put a 0 before it. So for example 2017-09-09 instead of 2017-9-9
+    if(dd < 10) {
+        dd = '0' + dd
+    }
+
+    if(mm < 10) {
+        mm = '0' + mm
+    }
+
+    return weekday[date.getDay()]+', ' + dd + '-' + mm + '-' + yyyy;
+}
+
+function clearExercises() {
+    //$("#all_exercises_container").find("*").off();
+    $("#all_exercises_container").off();
+    $("#all_exercises_container").html("");
+}
+
+function isInDoneExercises(value) {
+    return done_exercises.indexOf(value.toString()) > -1;
+}
+
+function addToDoneExercises(value) {
+    if(!isInDoneExercises(value.toString())){
+        done_exercises.push(value.toString());
+    }
+}
+
+function removeFromDoneExercises(value) {
+    if(isInDoneExercises(value.toString())){
+        var index = done_exercises.indexOf(value.toString());
+        if (index > -1) {
+            done_exercises.splice(index, 1);
+        }
+    }
 }
