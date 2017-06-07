0624420732443c3f1b35fff4893a521d7263b5f0
Desi <d.esi@hotmail.com>
2017-06-07 12:53:41 +0200

    Evaluation html and javascript update US#160

--- javascript/evaluation.js
+++ javascript/evaluation.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
+    var pain = document.getElementById('pain');
+    var paincount = document.getElementById('paincount');
+
     $("#improvementno").click(function() {
-        $('#pain').css("display","inline");
-        $('#paincount').css("display","inline");
+        $(pain).css("display","inline");
+        $(paincount).css("display","inline");
     });
     $("#improvementyes").click(function() {
-        if($('#pain').css('display') == 'inline')
+        if($(pain).css('display') == 'inline')
         {
-            $('#pain').css("display","none");
-            $('#paincount').css("display","none");
+            $(pain).css("display","none");
+            $(paincount).css("display","none");
         }
     });
     $("#nocomplaints").click(function() {
-        if($('#pain').css('display') == 'inline')
+        if($(pain).css('display') == 'inline')
         {
-            $('#pain').css("display","none");
-            $('#paincount').css("display","none");
+            $(pain).css("display","none");
+            $(paincount).css("display","none");
         }
     });
 });
