087be58a0d39ea72e87f004d88620910b9f60e5c
Desi <d.esi@hotmail.com>
2017-06-07 14:14:28 +0200

    Evaluation content updated US#250

--- javascript/evaluation-content.js
+++ javascript/evaluation-content.js
@@ -1,3 +1,31 @@
 /**
  * Created by larsw on 7-6-2017.
  */
+$(document).ready(function() {
+getQuestionAnswers();
+});
+function getQuestionAnswers() {
+    var request = $.ajax({
+        type: 'GET',
+        headers: {
+            'authorization': jwt
+        },
+        url: "http://localhost:8000" + "/treatment/exercises-day",
+        dataType: 'json',
+        statusCode: {
+            200: function () {
+                console.log(200, "succes!");
+            },
+            401: function (error) {
+                console.log(401);
+            },
+            404: function (error) {
+                console.log(404, error)
+            }
+        },
+        error: function (err) {
+            notifyUser("Kon geen oefeningen ophalen, neem contact op met uw systeembeheerder");
+            console.log("Error getting exercises: " + err.message);
+        }
+    });
+}
