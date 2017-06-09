8b66a6d37f6203c30c64e693a1fbfc8c00ff3394
maurice_2 <mauricebergboer@hotmail.com>
2017-06-09 13:02:43 +0200

    javascript file including ajax call for posting evaluation

--- javascript/ajax/evaluation/submit-evaluation.js
+++ javascript/ajax/evaluation/submit-evaluation.js
@@ -2,56 +2,123 @@
  * Ajax request for sending an evaluation-form filled in by a employee.
  *
  * Author: maurice_2.
- * Date: 31-5-2017.
+ * Date: 09-06-2017.
  */
-
 var jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJ1YmVuYXNzaW5rQGhvdG1haWwuY29tIiwidXNlcl9pZCI6NCwicm9sZV9pZCI6MCwiaWF0IjoxNDk1MzkzNTYwLCJleHAiOjE1MjY5Mjk1NjB9.4UMl25J0i7C4d5METeHxY-4FYrf9ez0B0RkkijuoaCc";
 
 $(document).ready(function () {
-    capture();
+
+    $('#submit-form').click(function () {
+        saveEvaluationData();
+    });
+
+    // capture(); // Screenshot with HTML2canvas
 });
 
-function capture() {
-    var element = $("#html-content-holder"); // global variable
-    var image_url = "";
+function saveEvaluationData(){
+    var formData = { answers: [] };
+    var questions = $("#questions").find('.question');
+    var checkedAns;
 
-    // SHOW SCREENSHOT
-    $("#btn-Preview-Image").on('click', function () {
-        html2canvas(element, {
-            onrendered: function (canvas) {
-//                    $("#previewImage").append(canvas);
-                image_url = canvas.toDataURL("image/png");
-                window.open(image_url);
-                alert("image_url: " + image_url);
+    for(i = 0; i < questions.length; i++){
+        var answer = {};
+        var currQuestion = questions[i];
+        var questType = currQuestion.classList[1]; // get question type, e.g.
 
-                $.ajax({
-                    type: 'POST',
-                    headers: {
-                        'authorization':jwt
-                    },
-                    url: "http://localhost:8000" + "/evaluation/add",
-                    // async: false,
-                    data: {
-                        "image_url": JSON.stringify(image_url)
-                    },
-                    dataType: 'text',
-                    statusCode: {
-                        201: function () {
-                            alert(201);
-                        },
-                        401: function (error) {
-                            alert(error);
-                        },
-                        400: function (error) {
-                            alert(error);
-                        }
-                    },
-                    error: function (err) {
-                        alert("Error: " + err);
-                    }
-                });
+        switch (questType) {
+            case "checkbox":
+                checkedAns = $(currQuestion).find('input[type=checkbox]:checked');
+                answer.checkbox = [];
+                for(i = 0; i < checkedAns.lenght; i++){
+                    var checked = $(checkedAns[i]).attr('choice').trim();
+                    answer.checkbox.push(checked);
+                }
+                break;
+            case "radio":
+                checkedAns = $(currQuestion).find('input[type=radio]:checked');
+                if(checkedAns != undefined) {
+                    answer.radio = checkedAns.attr('choice').trim();
+                }
+                break;
+        }
 
-            }
-        });
-    });
+        formData.answers.push(answer);
+    }
+
+    console.log(formData.answers);
+
+    postEvaluation(formData);
 }
+
+function postEvaluation(formData) {
+    $.ajax({
+        type: 'POST',
+        headers: {
+            'authorization': jwt
+        },
+        url: "http://localhost:8000" + "/evaluation/add",
+        data: formData,
+        success: function (data) {
+            location.replace("index.html");
+        },
+        statusCode: {
+            200: function () {
+                console.log(200);
+            },
+            401: function (error) {
+                console.log(401);
+            },
+            400: function (error) {
+                console.log(400, error)
+            }
+        },
+        error: function (err) {
+            console.log("Error posting evaluation");
+        }
+    })
+}
+
+// function capture() {
+//     var element = $("#html-content-holder"); // global variable
+//     var image_url = "";
+//
+//     // SHOW SCREENSHOT
+//     $("#btn-Preview-Image").on('click', function () {
+//         html2canvas(element, {
+//             onrendered: function (canvas) {
+// //                    $("#previewImage").append(canvas);
+//                 image_url = canvas.toDataURL("image/png");
+//                 window.open(image_url);
+//                 alert("image_url: " + image_url);
+//
+//                 $.ajax({
+//                     type: 'POST',
+//                     headers: {
+//                         'authorization':jwt
+//                     },
+//                     url: "http://localhost:8000" + "/evaluation/add",
+//                     // async: false,
+//                     data: {
+//                         "image_url": JSON.stringify(image_url)
+//                     },
+//                     dataType: 'text',
+//                     statusCode: {
+//                         201: function () {
+//                             alert(201);
+//                         },
+//                         401: function (error) {
+//                             alert(error);
+//                         },
+//                         400: function (error) {
+//                             alert(error);
+//                         }
+//                     },
+//                     error: function (err) {
+//                         alert("Error: " + err);
+//                     }
+//                 });
+//
+//             }
+//         });
+//     });
+// }
