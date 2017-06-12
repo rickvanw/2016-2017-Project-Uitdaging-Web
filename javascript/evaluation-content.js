518b03da0bbe40a5cb2aebb7bee4815dad4d040c
larswicherson <larswicherson@msn.com>
2017-06-12 09:30:04 +0200

    evaluation-content update printing done

--- javascript/evaluation-content.js
+++ javascript/evaluation-content.js
@@ -38,3 +38,14 @@ function userInteraction () {
         });
     }
 }
+
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
