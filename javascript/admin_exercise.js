d2c9c948feb7dbe066aacd8784041033bd58696b
rickvanw <rickvanweersel@hotmail.com>
2017-06-12 16:08:35 +0200

    US 270 - Fields editable when edit clicked

--- javascript/admin_exercise.js
+++ javascript/admin_exercise.js
@@ -6,12 +6,15 @@
 var jwt = sessionStorage.token;
 var done_exercises =[];
 var currentPage = 1;
+var amountOfPages;
 var hostAdress = "http://localhost:8000";
+getAmountOfExercises();
+
+// Get first page of exercises (first time)
 getExercises(currentPage);
 
 $(document).ready(function() {
     userInteraction();
-    $('.day_text').text("Pagina " + currentPage);
     $('.left_arrow').css('visibility', 'hidden');
 
     $('.link_collapse').css('visibility', 'hidden');
@@ -33,9 +36,12 @@ function userInteraction () {
         console.log("CLLICk");
         currentPage++;
         getExercises(currentPage);
-        $('.day_text').text("Pagina " + currentPage);
+        $('.day_text').text("Pagina " + currentPage + "/" + amountOfPages);
         if(currentPage>1){
             $('.left_arrow').css('visibility', 'visible');
+        }
+        if(currentPage==amountOfPages){
+            $('.right_arrow').css('visibility', 'hidden');
         }
     });
     $('.left_arrow').off("click").on("click", function (e) {
@@ -43,21 +49,23 @@ function userInteraction () {
         clearExercises();
         currentPage--;
         getExercises(currentPage);
-        $('.day_text').text("Pagina " + currentPage);
+        $('.day_text').text("Pagina " + currentPage + "/" + amountOfPages);
 
         if(currentPage==1){
             $('.left_arrow').css('visibility', 'hidden');
         }
-    });
-
-    $('.doneButton').off("click").on("click", function (e) {
-        e.stopImmediatePropagation();
-
-        return false;
+        if(currentPage<amountOfPages){
+            $('.right_arrow').css('visibility', 'visible');
+        }
     });
 
     $('.notDoneButton').off("click").on("click", function (e) {
         e.stopImmediatePropagation();
+
+        var buttonExerciseId=this.id.split("notDoneButton")[1];
+
+        $('#' + "exercise" + buttonExerciseId).find('.exercise_quickview_title').attr("contenteditable", "true");
+        $('#' + "exercise" + buttonExerciseId).find('.description_text').attr("contenteditable", "true");
 
         return false;
     });
@@ -68,10 +76,46 @@ function userInteraction () {
         return false;
     });
 
-    $('.dislikeButton').off("click").on("click", function (e) {
-        e.stopImmediatePropagation();
+}
 
+function getAmountOfExercises(){
+    var request = $.ajax({
+        type: 'GET',
+        headers: {
+            'authorization':jwt
+        },
+        url: hostAdress + "/exercise/rows",
+        dataType: 'json',
+        statusCode: {
+            200:function(){
+                console.log(200, "succes!");
+            },
+            401:function(error) {
+                console.log(401);
+            },
+            404: function(error){
+                console.log(404, error)
+            }
+        },
+        error: function (err) {
+            console.log("Error getting amount of exercises: " + err.message);
+        }
     });
+
+    request.done(function (data) {
+        var amountOfExercises = data[0]["count(*)"];
+        var pages =  amountOfExercises.toString()[0];
+
+        console.log("Modulus: "+ pages);
+        if((amountOfExercises % 10) > 0){
+            pages++;
+        }
+        console.log("Pages: "+ pages);
+
+        amountOfPages = pages;
+        $('.day_text').text("Pagina " + currentPage + "/" + amountOfPages);
+    });
+
 }
 
 // TODO, afhandelen wanneer geen oefening beschikbaar
@@ -120,10 +164,8 @@ function placeExercises(data) {
         $("#collapse0").attr("id", "collapse" + exercise.exercise_id);
         $("#link01").attr({href: "#collapse" + exercise.exercise_id, id: ""});
         $("#link02").attr({href: "#collapse" + exercise.exercise_id, id: ""});
-        $("#doneButton0").attr("id", "doneButton" + exercise.exercise_id);
         $("#notDoneButton0").attr("id", "notDoneButton" + exercise.exercise_id);
         $("#likeButton0").attr("id", "likeButton" + exercise.exercise_id);
-        $("#dislikeButton0").attr("id", "dislikeButton" + exercise.exercise_id);
         $("#exercise0").attr("id", "exercise" + exercise.exercise_id);
         $("#video0").attr("id", "video" + exercise.exercise_id);
 
@@ -148,94 +190,9 @@ function placeExercises(data) {
         //Repeats amount
         //TODO verander naar description uit database
         $('#' + "exercise" + exercise.exercise_id).find('.description_text').text("Planken is niet ingewikkeld. Voor de basisplank ga je eerst op je buik liggen.Plaats je ellebogen onder de schouders en zet je tenen in de vloer. Druk je bovenlichaam omhoog op je onderarmen en til ook je benen van de grond. Vind jehet lastig om in een keer je lijf omhoog te brengen, steun dan als tussenstap op je knieën.");
-
-        //Set previous like/dislike
-        if(exercise.rating_user == 1) {
-            $('#' + "exercise" + exercise.exercise_id).find('.likeButton').css('background-color', '#BFE5BF');
-        }else if(exercise.rating_user == -1){
-            $('#' + "exercise" + exercise.exercise_id).find('.dislikeButton').css('background-color', '#F99C9C');
-        }
-
-        //Set previous done/not done
-        if(exercise.done == 1) {
-            $('#' + "exercise" + exercise.exercise_id).find('.doneButton').css('background-color', '#BFE5BF');
-        }else if(exercise.done == -1){
-            $('#' + "exercise" + exercise.exercise_id).find('.notDoneButton').css('background-color', '#F99C9C');
-        }
-        if (exercise.done == 1) {
-            console.log("ID - " + exercise.exercise_id);
-            addToDoneExercises(exercise.exercise_id)
-        }
-        console.info(done_exercises);
     });
 
     userInteraction();
-}
-
-/**
- * Function for returning the current date.
- * @returns {Date}
- */
-function getCurrentDate(){
-    var currentDate = new Date();
-    var dd = currentDate.getDate();
-    var mm = currentDate.getMonth() + 1; // January is zero, so + 1
-    var yyyy = currentDate.getFullYear();
-
-    // If the days / months are smaller than 10, we want to put a 0 before it. So for example 2017-09-09 instead of 2017-9-9
-    if(dd < 10) {
-        dd = '0' + dd
-    }
-
-    if(mm < 10) {
-        mm = '0' + mm
-    }
-
-    currentDate = yyyy + '-' + mm + '-' + dd;
-    return currentDate;
-}
-
-function daysFromDate(daysBack) {
-    var today = new Date();
-    var yesterday = new Date(today);
-    yesterday.setDate(today.getDate() + daysBack);
-
-    var dd = yesterday.getDate();
-    var mm = yesterday.getMonth() + 1; // January is zero, so + 1
-    var yyyy = yesterday.getFullYear();
-
-    // If the days / months are smaller than 10, we want to put a 0 before it. So for example 2017-09-09 instead of 2017-9-9
-    if(dd < 10) {
-        dd = '0' + dd
-    }
-
-    if(mm < 10) {
-        mm = '0' + mm
-    }
-
-    return yyyy + '-' + mm + '-' + dd;
-}
-
-function getDateString(date){
-
-    var weekday = ["Zondag","Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag"];
-
-    date = new Date(date);
-
-    var dd = date.getDate();
-    var mm = date.getMonth() + 1; // January is zero, so + 1
-    var yyyy = date.getFullYear();
-
-    // If the days / months are smaller than 10, we want to put a 0 before it. So for example 2017-09-09 instead of 2017-9-9
-    if(dd < 10) {
-        dd = '0' + dd
-    }
-
-    if(mm < 10) {
-        mm = '0' + mm
-    }
-
-    return weekday[date.getDay()]+', ' + dd + '-' + mm + '-' + yyyy;
 }
 
 function clearExercises() {
