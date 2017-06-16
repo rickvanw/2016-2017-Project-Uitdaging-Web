5e7dd4f54bb2b947f74f525749283d5dd6716f6d
rickvanw <rickvanweersel@hotmail.com>
2017-06-16 16:00:42 +0200

    US 310 - Forget password modal with input/ button added on login page

---
+++ javascript/password-reset.js
@@ -0,0 +1,13 @@
+/**
+ * Created by rickv on 15-6-2017.
+ */
+$('.password_reset_button').off("click").on("click", function (e) {
+    e.stopImmediatePropagation();
+
+    var email = $('.email_input').val();
+
+
+
+    alert("HALLO " + email);
+
+});
