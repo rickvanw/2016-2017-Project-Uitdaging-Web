28222b22b5f74fcd61b7e8c79555ed8e2a2f0f2e
rubenassink <ruben_assink@hotmail.com>
2017-06-22 13:15:42 +0200

    US 320: Profiel bewerken // POST request for password-reset-req

--- javascript/password-reset.js
+++ javascript/password-reset.js
@@ -1,11 +1,28 @@
-/**
- * Created by rickv on 15-6-2017.
- */
-$('.password_reset_button').off("click").on("click", function (e) {
+
+$('#password_reset_button').off("click").on("click", function (e) {
     e.stopImmediatePropagation();
 
     var email = $('.email_input').val();
+    console.log(email);
 
-    alert("HALLO " + email);
+    $.ajax({
+        type: 'POST',
+        url: hostAdress + "/user/password-reset-req",
+        dataType: 'json',
+        data: {'email': email},
+        success: function (data) {
+        },
+        statusCode: {
+            201: function (data) {
+                window.location.href = "login.html";
+            }
+        },
+        error: function (err) {
+            if(err.status == 409) {
 
+
+            }
+            console.log("Error: " + err.message);
+        }
+    });
 });
